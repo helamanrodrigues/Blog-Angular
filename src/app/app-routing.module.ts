1e6ff88800b8a43ed2dce9bf0797bb7d261cd5cc
helamanrodrigues <u711602997@byui.edu>
2023-09-23 02:14:09 -0300

    Step Criano outras paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:HomeComponent
+    path:'', component:HomeComponent
   },
   {
     path:'content/:id',
