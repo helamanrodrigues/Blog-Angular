7960fecacacc9cef23f21a2fedd4a6004f50434c
helamanrodrigues <u711602997@byui.edu>
2023-09-26 00:47:20 -0300

    Step Deixando a content page dinamica

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake';
+
 
 @Component({
   selector: 'app-content',
@@ -11,14 +13,24 @@ export class ContentComponent implements OnInit {
   photoCover:string =""
   contentTitle:string =""
   contentDescription:string =""
+  private id:string | null = "0"
 
   constructor(
     private route:ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( value => console.log(value.get("id"))
-    )
+    this.route.paramMap.subscribe( value => this.id = value.get("id"))
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id = id ) [0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+
   }
 
 }
