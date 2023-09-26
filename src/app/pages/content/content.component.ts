42e67484f61ed84aa6715a91001f958aa0294d23
helamanrodrigues <u711602997@byui.edu>
2023-09-26 00:23:09 -0300

    Step Pegando os Dados Dinamicamente

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -9,11 +10,15 @@ export class ContentComponent implements OnInit {
 
   photoCover:string =""
   contentTitle:string =""
-  contentDescription:string = ""
+  contentDescription:string =""
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value => console.log(value.get("id"))
+    )
   }
 
 }
